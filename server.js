8a40115e7050cd7ea037919b5f5f456dd972626b
ironrabb <gimmyeongil641@gmail.com>
2026-08-21 14:36:59 +0900

    BRR: building express web server

---
+++ server.js
@@ -0,0 +1,25 @@
+console.log(" web serverni boshlash");
+const express = require("express");
+const http = require("http");
+const app = express();
+
+// 1 KRISH KODLARI
+app.use(express.static("public")); // kirib kelayotgan malumotlar uchun faqt public folderi ochiq degan manoni bldradi
+app.use(express.json()); // json formatga ogirib beradi
+app.use(express.urlencoded({ extended: true }));
+// SEESIONGA BOGLIQ KODLAR
+// 3 VIEWSGA BOGLIQ KODLAR
+app.set("views", "views");
+app.set("view ejs", "ejs");
+// 4 TOUTING KODLAR
+app.get("/hello", function (req, res) {
+  res.end(`<h1 style="background: blue ">HELLO   WORLD</h1>`);
+});
+app.get("/gift", function (req, res) {
+  res.end(`<h1 style="background: yellow ">siz sovgalar bolimidasz</h1>`);
+});
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+  console.log(`the server is running succesfull on port: ${PORT}`);
+});
