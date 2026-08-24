245253f5148ad703f4c04d4d552c08b54fff0570
ironrabb <gimmyeongil641@gmail.com>
2026-08-24 13:10:56 +0900

    forma va EJS qo'shildi

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ console.log(" web serverni boshlash");
 const express = require("express");
 const http = require("http");
 const app = express();
-
+// const res = require("express/lib/response");
 // 1 KRISH KODLARI
 app.use(express.static("public")); // kirib kelayotgan malumotlar uchun faqt public folderi ochiq degan manoni bldradi
 app.use(express.json()); // json formatga ogirib beradi
@@ -10,16 +10,25 @@ app.use(express.urlencoded({ extended: true }));
 // SEESIONGA BOGLIQ KODLAR
 // 3 VIEWSGA BOGLIQ KODLAR
 app.set("views", "views");
-app.set("view ejs", "ejs");
+app.set("view engine", "ejs");
 // 4 ROUTING KODLAR
-app.get("/hello", function (req, res) {
-  res.end(`<h1 style="background: blue ">HELLO   WORLD</h1>`);
+// app.get("/hello", function (req, res) {
+//   res.end(`<h1 style="background: blue ">HELLO   WORLD</h1>`);
+// });
+// app.get("/gift", function (req, res) {
+//   res.end(`<h1 style="background: yellow ">siz sovgalar bolimidasz</h1>`);
+// });
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "succes" }); //json shalida malumotni qaytarish
 });
-app.get("/gift", function (req, res) {
-  res.end(`<h1 style="background: yellow ">siz sovgalar bolimidasz</h1>`);
+app.get("/", function (req, res) {
+  res.render("harid");
 });
+
+//
 const server = http.createServer(app);
-let PORT = 3000;
+let PORT = 3001;
 server.listen(PORT, function () {
   console.log(`the server is running succesfull on port: ${PORT}`);
 });
