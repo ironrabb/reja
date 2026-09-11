f65c9ab8354bddc876559e5051b6320b1e6aa543
ironrabb <gimmyeongil641@gmail.com>
2026-09-11 17:27:51 +0900

    yangi

--- app.js
+++ app.js
@@ -1,60 +1,32 @@
 console.log(" web serverni boshlash");
 const express = require("express");
-// const mongodb = require("mongodb");
 const { ObjectId } = require("mongodb");
 const app = express();
 const fs = require("fs");
 let user;
+
 fs.readFile("database/user.json", "utf8", (err, data) => {
-  // ✅ Asinxron — fayl o'qish (to'xtamaydi)
   if (err) {
     console.log("ERROR", err);
   } else {
     user = JSON.parse(data);
   }
 });
-// MONGO DB CHAQIRISH
-const db = require("./server.js").getDb(); // ✅ TO'G'RI
-// const res = require("express/lib/response");
-// 1 KRISH KODLARI
-app.use(express.static("public")); // kirib kelayotgan malumotlar uchun faqt public folderi ochiq degan manoni bldradi
-app.use(express.json()); // json formatga ogirib beradi
-app.use(express.urlencoded({ extended: true })); // Forma ma'lumotlarini o'qish
-// SEESIONGA BOGLIQ KODLAR
-// 3 VIEWSGA BOGLIQ KODLAR
+
+const db = require("./server.js").getDb();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", "views");
 app.set("view engine", "ejs");
-// 4 ROUTING KODLAR
-// app.get("/hello", function (req, res) {
-//   res.end(`<h1 style="background: blue ">HELLO   WORLD</h1>`);
-// });
-// app.get("/gift", function (req, res) {
-//   res.end(`<h1 style="background: yellow ">siz sovgalar bolimidasz</h1>`);
-// });
-// app.post("/create-item", (req, res) => {
-//   console.log(req.body);
-//   // res.json({ test: "succes" }); //json shalida malumotni qaytarish
-//   const new_reja = req.body.reja;
-//   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-//     if (err) {
-//       console.log(err);
-//       res.end("somthing went wrong");
-//     } else {
-//       res.end("succesfully added");
-//     }
-//   });
-// });
+
 app.post("/create-item", async (req, res) => {
   const new_reja = req.body.reja;
-
   try {
     const result = await db.collection("plans").insertOne({ reja: new_reja });
-    console.log(result);
-
-    res.json({
-      _id: result.insertedId,
-      reja: new_reja,
-    });
+    res.json({ _id: result.insertedId, reja: new_reja });
   } catch (err) {
     console.log(err);
     res.status(500).json({ success: false, message: "somthing went wrong" });
@@ -63,7 +35,6 @@ app.post("/create-item", async (req, res) => {
 
 app.post("/delete-item", async (req, res) => {
   const id = req.body.id;
-
   try {
     const result = await db
       .collection("plans")
@@ -74,10 +45,10 @@ app.post("/delete-item", async (req, res) => {
     res.status(500).json({ success: false, message: "somthing went wrong" });
   }
 });
+
 app.post("/edit-item", async (req, res) => {
   const id = req.body.id;
   const new_reja = req.body.reja;
-
   try {
     const result = await db
       .collection("plans")
@@ -88,6 +59,7 @@ app.post("/edit-item", async (req, res) => {
     res.status(500).json({ success: false, message: "somthing went wrong" });
   }
 });
+
 app.post("/delete-all", async (req, res) => {
   try {
     const result = await db.collection("plans").deleteMany({});
@@ -97,31 +69,18 @@ app.post("/delete-all", async (req, res) => {
     res.status(500).json({ success: false, message: "somthing went wrong" });
   }
 });
-// app.get("/", function (req, res) {
-//   db.collection("plans")
-//     .find()
-//     .toArray((err, data) => {
-//       if (err) {
-//         console.log(err);
-//         res.end("something went wrong");
-//       } else {
-//         console.log(data);
-//         res.render("reja");
-//       }
-//     });
-//   res.render("reja");
-// });
+
 app.get("/", async (req, res) => {
   console.log("user enterred /");
   try {
     const data = await db.collection("plans").find().toArray();
-    // console.log(data);
     res.render("reja", { items: data });
   } catch (err) {
     console.log(err);
     res.end("something went wrong");
   }
 });
+
 app.get("/author", (req, res) => {
   res.render("author", { user: user });
 });
--- server.js
+++ server.js
@@ -1,66 +1,32 @@
-// console.log("heyy");
-// const http = require("http");
-// const mongodb = require("mongodb");
-
-// // mongo db ulash fZy2iLQKsCFe7tN3
-// let db;
-// const connectionString =
-//   "mongodb+srv://gimmyeongil641_db_user:fZy2iLQKsCFe7tN3@cluster0.wlegmsp.mongodb.net/?appName=Cluster0";
-// console.log(JSON.stringify(connectionString));
-// mongodb.connect(
-//   connectionString,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-//   (err, client) => {
-//     if (err) {
-//       console.log("error on connection mongo db:", err); // to'liq xato matni
-//     } else {
-//       console.log("mongo db ulanmoqda");
-//       db = client.db("REJA");
-
-//       const app = require("./app.js");
-//       const server = http.createServer(app);
-//       let PORT = 3010;
-//       server.listen(PORT, function () {
-//         console.log(
-//           `the server is running succesfull on port: ${PORT}, http://localhost:${PORT}`,
-//         );
-//       });
-//     }
-//   },
-// );
-
-console.log("heyy");
+console.log("web serverni boshlash");
 const http = require("http");
 const { MongoClient } = require("mongodb");
 
 const connectionString =
-  "mongodb+srv://gimmyeongil641_db_user:fZy2iLQKsCFe7tN3@cluster0.wlegmsp.mongodb.net/REJA";
-
-const client = new MongoClient(connectionString);
+  "mongodb+srv://gimmyeongil641_db_user:fZy2iLQKsCFe7tN3@cluster0.wlegmsp.mongodb.net/REJA?retryWrites=true&w=majority";
 
 let db;
 
-client
-  .connect()
-  .then(() => {
+MongoClient.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((client) => {
     console.log("mongo db ulandi");
-    // console.log(client);
     db = client.db("REJA"); // baza nomi
 
     const app = require("./app.js");
     const server = http.createServer(app);
-    let PORT = 3010;
+    const PORT = 9002; // ❗ bu yerga ustoz sizga bergan portni yozing
+
     server.listen(PORT, function () {
       console.log(
-        `the server is running succesfull on port: ${PORT}, http://localhost:${PORT}`,
+        `the server is running succesfully on port: ${PORT}, http://localhost:${PORT}`,
       );
     });
   })
   .catch((err) => {
     console.log("error on connection mongo db:", err);
   });
+
 module.exports = { getDb: () => db };
-// module.exports = client;
